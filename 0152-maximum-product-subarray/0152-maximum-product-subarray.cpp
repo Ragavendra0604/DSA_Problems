6c071e890ae03dbe30176b44a432f3cd2dfe3c71
Ragavendra <198088826+Ragavendra0604@users.noreply.github.com>
2026-01-14 00:24:20 +0530

    Time: 0 ms (100%), Space: 17.8 MB (23.04%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -5,12 +5,16 @@ public:
 
         int maxProd = INT_MIN;
 
+        int prefix = 1;
+        int suffix = 1;
+
         for(int i=0;i<n;i++){
-            int prod = 1;
-            for(int j=i;j<n;j++){
-                prod *= nums[j];
-                maxProd = max(prod, maxProd);
-            }
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+
+            prefix *= nums[i];
+            suffix *= nums[n-1-i];
+            maxProd = max(maxProd, max(prefix, suffix));
         }
         return maxProd;
     }
