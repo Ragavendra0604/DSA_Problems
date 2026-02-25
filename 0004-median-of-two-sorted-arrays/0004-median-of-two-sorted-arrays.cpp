f6f6c8bc0720f4343ba42036ac4aa13d03eb08b9
Ragavendra <198088826+Ragavendra0604@users.noreply.github.com>
2026-02-25 08:43:14 +0530

    Time: 0 ms (100%), Space: 95.2 MB (44.73%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> rs;
+        if(nums2.size() < nums1.size()) return findMedianSortedArrays(nums2, nums1);
 
-        int i = 0, m = nums1.size();
-        int j = 0, n = nums2.size();
+        int n1 = nums1.size();
+        int n2 = nums2.size();
 
-        while(i < m && j < n){
-            if(nums1[i] < nums2[j]){
-                rs.push_back(nums1[i]);
-                i++;
+        int low = 0, high = n1;
+
+        while(low <= high){
+            int cut1 = (low + high) >> 1;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
+
+            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+
+            if(left1 <= right2 && left2 <= right1){
+                if((n1 + n2) % 2 == 0){
+                    return (double)(max(left1, left2) + min(right1, right2)) / 2.0;
+                }
+                else {
+                    return (double)max(left1, left2);
+                }
             }
-            else if(nums1[i] > nums2[j]){
-                rs.push_back(nums2[j]);
-                j++;
+            else if(left1 > right2){
+                high = cut1 - 1;
             }
-            else{
-                continue;
+            else {
+                low = cut1 + 1;
             }
         }
-
-        while(i < m){
-            rs.push_back(nums1[i]);
-            i++;
-        }
-        while(j < n){
-            rs.push_back(nums2[j]);
-            j++;
-        }
-
-        int len = rs.size();
-
-        if(len % 2 == 0){
-            int mid = (0 + len) / 2;
-            return (double)(rs[mid-1] + rs[mid])/2;
-        }
-
-        else{
-            return (double)rs[len/2];
-        }
+        return 0.0;
     }
 };
