b1851c27324469049b0dd260a6f214b4933fb1ef
HARIRAGAVENDRA M G <198088826+Ragavendra0604@users.noreply.github.com>
2026-06-28 22:57:40 +0530

    Time: 0 ms (100%), Space: 8.4 MB (92.9%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double solve(double x, long n) {
+        if (n == 0) return 1.0;
+
+        if (n == 1) return x;
+
+        if (n % 2 == 0) {
+            return solve(x * x, n / 2);
+        }
+        return x * solve(x, n - 1);
+    }
+    double myPow(double x, int n) {
+        double m = n;
+        if(m < 0){
+            return (1.0 / solve(x, -1 * m));
+        }
+        return solve(x, n);
+    }
+};
