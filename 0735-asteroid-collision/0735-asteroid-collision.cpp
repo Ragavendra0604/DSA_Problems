4dc5d515a2869e19f8b05d6917e29854f2377031
HARIRAGAVENDRA M G <198088826+Ragavendra0604@users.noreply.github.com>
2026-07-23 00:06:55 +0530

    Time: 0 ms (100%), Space: 21.4 MB (97.36%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;
+
+        int n = asteroids.size();
+
+        for (int x : asteroids) {
+
+            if (x > 0) {
+                st.push_back(x);
+            }
+            else {
+
+                while (!st.empty() &&
+                       st.back() > 0 &&
+                       st.back() < abs(x)) {
+                    st.pop_back();
+                }
+
+                if (!st.empty() &&
+                    st.back() == abs(x)) {
+                    st.pop_back();
+                }
+                else if (st.empty() ||
+                         st.back() < 0) {
+                    st.push_back(x);
+                }
+            }
+        }
+        return st;
+    }
+};
