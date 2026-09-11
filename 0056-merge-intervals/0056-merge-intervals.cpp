db7578ee56be551e895b99ba01d9d043ecd5620d
HARIRAGAVENDRA M G <198088826+Ragavendra0604@users.noreply.github.com>
2026-09-11 23:17:21 +0530

    Time: 8 ms (24.66%), Space: 25 MB (5.67%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
+        vector<vector<int>> mergedIntervals;
+        if(intervals.size() == 0){
+            return mergedIntervals;
+        }
+        
         sort(intervals.begin(), intervals.end());
 
-        vector<vector<int>> ans;
+        vector<int> tempIntervals = intervals[0];
 
-        for(int i=0;i<n;i++){
-            if(ans.empty() || intervals[i][0] > ans.back()[1]){
-                ans.push_back(intervals[i]);
+        for(auto it : intervals){
+            if(it[0] <= tempIntervals[1]){
+                tempIntervals[1] = max(it[1], tempIntervals[1]);
             }
             else{
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+                mergedIntervals.push_back(tempIntervals);
+                tempIntervals = it;
             }
         }
-        return ans;
+        mergedIntervals.push_back(tempIntervals);
+
+        return mergedIntervals;
     }
 };
