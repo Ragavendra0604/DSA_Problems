27f2b801d3643059a230b9efb15f68cf589b8e2c
HARIRAGAVENDRA M G <198088826+Ragavendra0604@users.noreply.github.com>
2026-09-16 00:02:11 +0530

    Time: 0 ms (100%), Space: 8.2 MB (11.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -10,17 +10,29 @@ public:
     // }
     
     // 2. Memoization
-    int helper(int n, vector<int> &dp){
-        if(n <= 1){
-            return n;
-        }
-        if(dp[n] != -1) return dp[n];
+    // int helper(int n, vector<int> &dp){
+    //     if(n <= 1){
+    //         return n;
+    //     }
+    //     if(dp[n] != -1) return dp[n];
 
-        return dp[n] = fib(n - 1) + fib(n - 2);
-    }
-    int fib(int n) {
-        vector<int> dp(n + 1, -1);
-        dp[n] = helper(n, dp);
+    //     return dp[n] = fib(n - 1) + fib(n - 2);
+    // }
+    // int fib(int n) {
+    //     vector<int> dp(n + 1, -1);
+    //     dp[n] = helper(n, dp);
+    //     return dp[n];
+    // }
+
+    // 3. Tabulation
+    int fib(int n){
+        if(n <= 1) return n;
+        vector<int> dp(n+1, 0);
+        dp[0] = 0, dp[1] = 1;
+        for(int i = 2 ; i <= n ; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
         return dp[n];
     }
 };
