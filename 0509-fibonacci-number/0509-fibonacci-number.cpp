0f8171993dab877505ad1f5be128d21226210c2e
HARIRAGAVENDRA M G <198088826+Ragavendra0604@users.noreply.github.com>
2026-09-15 23:59:33 +0530

    Time: 2347 ms (22.46%), Space: 364.8 MB (11.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
-    int fib(int n) {
+    // 1. Recursion
+    // int fib(int n) {
+    //     if(n <= 1){
+    //         return n;
+    //     }
+
+    //     return fib(n - 1) + fib(n - 2);
+    // }
+    
+    // 2. Memoization
+    int helper(int n, vector<int> &dp){
         if(n <= 1){
             return n;
         }
+        if(dp[n] != -1) return dp[n];
 
-        return fib(n - 1) + fib(n - 2);
+        return dp[n] = fib(n - 1) + fib(n - 2);
+    }
+    int fib(int n) {
+        vector<int> dp(n + 1, -1);
+        dp[n] = helper(n, dp);
+        return dp[n];
     }
 };
