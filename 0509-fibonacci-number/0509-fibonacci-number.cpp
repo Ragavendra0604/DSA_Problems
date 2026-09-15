9c86d8f807bd1f7c4da2f59af468a98aaaafbe10
HARIRAGAVENDRA M G <198088826+Ragavendra0604@users.noreply.github.com>
2026-09-16 00:04:43 +0530

    Time: 2 ms (77.38%), Space: 7.9 MB (37.82%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -25,14 +25,30 @@ public:
     // }
 
     // 3. Tabulation
+    // int fib(int n){
+    //     if(n <= 1) return n;
+    //     vector<int> dp(n+1, 0);
+    //     dp[0] = 0, dp[1] = 1;
+    //     for(int i = 2 ; i <= n ; i++){
+    //         dp[i] = dp[i - 1] + dp[i - 2];
+    //     }
+
+    //     return dp[n];
+    // }
+
+    // 4. Optimize Space to constant
     int fib(int n){
         if(n <= 1) return n;
-        vector<int> dp(n+1, 0);
-        dp[0] = 0, dp[1] = 1;
+        int prev2 = 0;
+        int prev1 = 1;
+
+        int curr = 0;
         for(int i = 2 ; i <= n ; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return dp[n];
+        return prev1;
     }
 };
