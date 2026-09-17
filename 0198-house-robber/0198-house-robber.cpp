2cf64ce5277d154ead0a949bdf510772464e5fcd
HARIRAGAVENDRA M G <198088826+Ragavendra0604@users.noreply.github.com>
2026-09-17 23:58:30 +0530

    Time: 1 ms (9.29%), Space: 10.8 MB (23.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,53 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int prev1 = 0;
-        int prev2 = 0;
+    // 1. Recursion
+    // int helper(int idx, vector<int> &nums){
+    //     if(idx == 0) return nums[idx];
 
-        for(int num: nums){
-            int curr = max(prev1, prev2+num);
-            prev2 = prev1;
-            prev1 = curr;
+    //     if(idx < 0) return 0;
+
+    //     int pick = nums[idx] + helper(idx - 2, nums);
+
+    //     int nopick = 0 + helper(idx - 1, nums);
+
+    //     return max(pick, nopick);
+    // }
+
+    // 2. Memoization
+    // int helper(int idx, vector<int> &nums, vector<int> &dp){
+    //     if(idx == 0) return nums[idx];
+
+    //     if(idx < 0) return 0;
+
+    //     if(dp[idx] != -1) return dp[idx];
+
+    //     int pick = nums[idx] + helper(idx - 2, nums, dp);
+
+    //     int nopick = 0 + helper(idx - 1, nums, dp);
+
+    //     return dp[idx] = max(pick, nopick);
+    // }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        // 2. Memoization
+        // vector<int> dp(n + 1, -1);
+        // int maxMoney = helper(n - 1, nums, dp);
+
+        // return dp[n - 1];
+        
+        // 3. Tabulation
+        vector<int> dp(n); 
+
+        dp[0] = nums[0];
+        int neg = 0;
+
+        for(int i = 1 ; i < n ; i++){
+            int pick = nums[i]; if(i > 1) pick += dp[i - 2];
+            int notpick = 0 + dp[i - 1];
+
+            dp[i] = max(pick, notpick);
         }
-        return prev1;
+        return dp[n - 1];
     }
 };
